3b0c288273504579c6a6b0b4c23780e07205b2da
Wasif Hussain <95441107+Wasif0787@users.noreply.github.com>
2023-10-26 14:09:31 +0530

    Added process.env.port in app.js

--- app.js
+++ app.js
@@ -211,6 +211,6 @@ app.post("/submit", function (req, res) {
     })
 })
 
-app.listen(3000, function () {
+app.listen(3000 || process.env.PORT, function () {
     console.log("Server started on port 3000");
 });
